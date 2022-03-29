18c836ef4aead2a82f6b555c28a46c06311df2e4
Max Cotton <mcotton@mxnet.ca>
2022-03-29 00:09:53 -0400

    clean up file

--- diagrams/devops_ongoing.py
+++ diagrams/devops_ongoing.py
@@ -13,23 +13,7 @@ from diagrams.onprem.iac import Terraform
 
 with Diagram(name="DevOps Ongoing Process", filename="devops_ongoing", show=False):
   ingress = Github("GitHub Actions")
-  # portainer = Custom ("Portainer", "./local_resources/portainer.png")
-  # terraform = Custom("Terraform", "./local_resources/terraform.png")
-  # packer = Custom("Packer", "./local_resources/packer.png")
-  # ansible = Ansible("Ansible")
   runner = Ubuntu("Runner")
-
-  # with Cluster("Servers"):
-  #   as01 = Vmware("AS01")
-  #   as02 = Vmware("AS02")
-  #   ingress >> packer >> terraform
-  #   terraform >> as01
-  #   terraform >> as02
-
-  #   with Cluster("GitHub Runner"):
-  #
-  #     terraform >> runner
-  #     ingress >> runner
 
   with Cluster("Docker"):
     # runner >> ansible
